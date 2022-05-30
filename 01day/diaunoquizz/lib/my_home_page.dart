8b8b1a20b580d8b859e8e291ed7182cebb46f390
miguelgarcia96 <masbung96@gmail.com>
2022-05-30 16:40:03 -0500

    Add stateful widget

--- 01day/diaunoquizz/lib/my_home_page.dart
+++ 01day/diaunoquizz/lib/my_home_page.dart
@@ -1,12 +1,21 @@
+// ignore_for_file: avoid_print
+
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-  MyHomePage({Key? key}) : super(key: key);
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
 
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
   var questionIdex = 0;
 
   void answerQuestion() {    
-    questionIdex++;
+    setState(() {
+      questionIdex++;
+    });    
     print("Answer question");
     print("questionIdex: $questionIdex");
   }
