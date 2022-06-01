463d9740cafec4c93dadf112ef044e4389687eb1
miguelgarcia96 <masbung96@gmail.com>
2022-06-01 10:44:30 -0500

    Add transaction model and data

---
+++ 01day/diaunogastos/lib/data/transaction_data.dart
@@ -0,0 +1,22 @@
+import 'package:diaunogastos/models/transaction.dart';
+
+final List<Transaction> transactions = [
+  Transaction(
+    id: "1",
+    title: "Gas",
+    amount: 200,
+    date: DateTime.now(),
+  ),
+  Transaction(
+    id: "2",
+    title: "Shoes",
+    amount: 100,
+    date: DateTime.now(),
+  ),
+  Transaction(
+    id: "3",
+    title: "Food",
+    amount: 300,
+    date: DateTime.now(),
+  ),  
+];
