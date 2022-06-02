ef2bed85ce6dd9c04415ecf678d3a3f0933c7455
miguelgarcia96 <masbung96@gmail.com>
2022-06-02 11:20:13 -0500

    Add FloattingActionButton

--- 01day/diaunogastos/lib/my_home_page.dart
+++ 01day/diaunogastos/lib/my_home_page.dart
@@ -12,19 +12,23 @@ class MyHomePage extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Card(
-            child: SizedBox(
-              width: 200,
-              child: Text("Chart"),
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Card(
+              child: SizedBox(
+                width: 200,
+                child: Text("Chart"),
+              ),
             ),
-          ),
-          UserTransactionState(),
-        ],
+            UserTransactionState(),
+          ],
+        ),
       ),
-      ),      
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
