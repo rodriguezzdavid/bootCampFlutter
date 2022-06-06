4c84035a5fc84affec19138198f96637464e3e70
miguelgarcia96 <masbung96@gmail.com>
2022-06-06 10:35:18 -0500

    Add products.json

---
+++ 02day/getxlearning/lib/controllers/product_controller.dart
@@ -0,0 +1,6 @@
+import 'package:get/get.dart';
+import 'package:getxlearning/models/product.dart';
+
+class ProductController extends GetxController {
+  List<Product> _products = [];
+}
---
+++ 02day/getxlearning/lib/models/product.dart
@@ -0,0 +1,13 @@
+class Product {
+  final int id;
+  final String name, description;
+  final double price, rate;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.rate,
+  });
+}
