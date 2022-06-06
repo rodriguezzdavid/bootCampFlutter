3a0193c5e01be19555b226cc353e9558c61c2022
miguelgarcia96 <masbung96@gmail.com>
2022-06-06 11:31:33 -0500

    Send data between pages

--- 02day/getxlearning/lib/controllers/user_details_controller.dart
+++ 02day/getxlearning/lib/controllers/user_details_controller.dart
@@ -1,5 +1,12 @@
 import 'package:get/get.dart';
+import 'package:getxlearning/models/app_user.dart';
 
-class UserDetailsControlller extends GetxController {
-  
+class UserDetailsController extends GetxController {
+  AppUser? user;
+
+  @override
+  void onInit() {    
+    super.onInit();
+    user = Get.arguments as AppUser;
+  }
 }
--- 02day/getxlearning/lib/pages/user_details_page.dart
+++ 02day/getxlearning/lib/pages/user_details_page.dart
@@ -1,17 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:getxlearning/controllers/user_details_controller.dart';
 
 class UserDetailsPage extends StatelessWidget {
   const UserDetailsPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("User details"),
-      ),
-      body: const Center(
-        child: Text("Nombre del usuario"),
-      ),
+    return GetBuilder<UserDetailsController>(
+      init: UserDetailsController(),
+      builder: (userDetailsController) {
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text("User details"),
+          ),
+          body: Center(
+            child: Text(userDetailsController.user!.firstName),
+          ),
+        );
+      }
     );
   }
 }
